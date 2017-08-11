43006e2caf6a9d86ece39d20ffca2bc43f382545
boul <boul@boul-HP-ProBook-450-G3>
2017-08-11 15:49:53 +0200

    test color google maps

--- index.php
+++ index.php
@@ -4,25 +4,31 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-            
+
         <!-- libaries css-->
-            <link type="text/css" rel="stylesheet" href="libraries/bootstrap4/css/bootstrap.css" />
+        <link type="text/css" rel="stylesheet" href="libraries/bootstrap4/css/bootstrap.css" />
 
         <!-- libraries js -->        
-            <script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
-            <script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
-            <script type="text/javascript" src="libraries/bootstrap4/js/bootstrap.js"></script>
-            
+        <script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
+        <script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
+        <script type="text/javascript" src="libraries/bootstrap4/js/bootstrap.js"></script>
+
         <!-- custom css  -->
-            <link type="text/css" rel="stylesheet" href="css/style.css" />
-        
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+
         <!-- custom js  -->
-            <script type="text/javascript" src="js/carrousel/main.js"></script>
-        
+        <script type="text/javascript" src="js/carrousel/main.js"></script>
+
     </head>
     <body>
+
+        <div id="google_maps" class="container">
+            <h2>Où nous trouver?</h2>
+            <div class="row">
+                <iframe class="col-8 offset-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2813.320378635347!2d1.5125726157830155!3d45.1603618616864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f8bd6a2870a2e3%3A0x885870a981b50e86!2sRue+Romain+Rolland%2C+19100+Brive-la-Gaillarde!5e0!3m2!1sfr!2sfr!4v1502457260038" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></iframe>
+            </div>
+        </div>
         <?php
-        // put your code here
         ?>
     </body>
 </html>
