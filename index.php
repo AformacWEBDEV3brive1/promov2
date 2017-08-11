ae21ed407c08058feea7609323ebb9c938622e21
okli <okli@nomads>
2017-08-11 12:48:59 +0200

    > ajout dans le projet des libraries telter / bootstrap / jquery > ajout dans le head des librairies necessaires

--- index.php
+++ index.php
@@ -8,6 +8,21 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+            
+        <!-- libaries css-->
+            <link type="text/css" rel="stylesheet" href="libraries/bootstrap4/css/bootstrap.css" />
+
+        <!-- libraries js -->        
+            <script type="text/javascript" src="libraries/jQuery/jquery-3.2.1.js"></script>
+            <script type="text/javascript" src="libraries/tether/dist/js/tether.js"></script>
+            <script type="text/javascript" src="libraries/bootstrap4/js/bootstrap.js"></script>
+            
+        <!-- custom css  -->
+            <link type="text/css" rel="stylesheet" href="css/style.css" />
+        
+        <!-- custom js  -->
+            <script type="text/javascript" src="js/carrousel/main.js"></script>
+        
     </head>
     <body>
         <?php
