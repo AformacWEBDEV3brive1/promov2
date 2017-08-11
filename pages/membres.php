040905c47dc2a6f3a9de55b4f12ff81d6a75da4c
rackmaninov <rackmaninov@racmaninov>
2017-08-11 15:52:57 +0200

    test bootstrap ^^

---
+++ pages/membres.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link rel="stylesheet" href="../css/style.css"/>
+        <link type="text/css" rel="stylesheet" href="../libraries/bootstrap4/css/bootstrap.css" />
+        <meta charset="utf-8" />
+
+        <title>Page_Membre</title>
+
+    </head>
+
+
+    <body id="member_page">
+        <div class="container">
+            <div class="row">   
+                <div col-12 col-md-6>
+                    <p>sdgsdgsdg</p>
+                </div>
+
+                <div col-12 col-md-6>
+                    <p>sdgsdgsd</p>
+                </div>
+                </div>
+            <div class="row">   
+                <div col-12 col-md-6>
+                    <p>sgsdgsdg</p>
+                </div>
+            
+           
+                <div col-12 col-md-6>
+                    <p>gdfgdfgdf</p>
+                </div>
+            </div>
+             <div class="row">
+                <div col-12 col-md-6>
+                    <p>dfgdfdfgg</p>
+                </div>
+                <div col-12 col-md-6>
+                    <p>dgfgdfg</p>
+                </div>
+
+            </div>
+        </div>
+    </body>
+
+</html>
+
