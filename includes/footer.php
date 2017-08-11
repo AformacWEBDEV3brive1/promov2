10a8fc609894372b023639e6796510a27864dd55
nathaniel <nathaniel@AOC>
2017-08-11 15:50:23 +0200

    Merge origin/master
    Conflicts:
    	Japon/public_html/index.html
    	Japon/public_html/javascript.js
    	Japon/public_html/style.css
    
    test de scss sass includes

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+        <div id='includes_header'>
+            <div>
+                <h1>YOLOOOOOOOOOOOOOOOO!!!!!!!!!!!</h1>
+            </div>
+            <div>
+                <h2>Promo</h2>
+            </div>
+            <div>
+                <label>
+                    <p>
+                        Bonjour je m'appel jen qul ta maire.
+                    </p>
+                </label>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <script type="text/javascript" src="../libraries/tether/dist/js/tether.js"></script>
+        <script type="text/javascript" src="../libraries/bootstrap4/js/bootstrap.js"></script>
+        <script src="../libraries/jQuery/jquery-3.2.1.js"></script>
+        <link rel="stylesheet" href="../libraries/bootstrap/css/bootstrap.css"/> 
+        <link rel='stylesheet' href='../css/style.css'/>
+    </head>
+    <body>
+        <div id='includes_header'>
+            <div>
+                <h1>YOLOOOOOOOOOOOOOOOO!!!!!!!!!!!</h1>
+            </div>
+            <div>
+                <h2>Promo</h2>
+            </div>
+            <div>
+                <label>
+                    <p>
+                        Bonjour je m'appel jen qul ta maire.
+                    </p>
+                </label>
+            </div>
+        </div>
+<?php
+include 'footer.php';
+?>
